e318d64d62f8b6280a931071d074fe7f05eb5854
Abhiram <abhiramap05@gmail.com>
2026-09-20 19:32:52 +0530

    added run_migrationsmethod

--- src/ingestion_layer/database/db.py
+++ src/ingestion_layer/database/db.py
@@ -3,6 +3,9 @@ from sqlalchemy.orm import sessionmaker
 from ..models.model import Base
 
 
+from .migrate import run_migrations
+
+
 DB_URL="sqlite:///./ecogen.db"
 
 engine=create_engine(DB_URL,connect_args={"check_same_thread":False})
@@ -10,6 +13,7 @@ SessionLocal=sessionmaker(bind=engine)
 
 def init_database():
     Base.metadata.create_all(bind=engine)
+    run_migrations("ecogen.db")
 
 def get_database():
     db=SessionLocal()
